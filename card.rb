eb6a953c9d675d5063a3f5ca860204bbf48ad1d4
Daniel Carter <geddoe@gmail.com>
2013-11-29 14:58:49 -0500

    Simple functionality for cards and deck.

--- card.rb
+++ card.rb
@@ -1,3 +1,8 @@
 class Card
+  attr_reader :front,:back
 
+  def initiaize(front,back)
+    @front = front
+    @back = back
+  end
 end
--- deck.rb
+++ deck.rb
@@ -1,3 +1,14 @@
 class Deck
 
+  def initialize
+    @deck = []
+  end
+
+  def add_card(card)
+    @deck << card
+  end
+
+  def review(num_to_review)
+    @deck.sample(num_to_review)
+  end
 end
